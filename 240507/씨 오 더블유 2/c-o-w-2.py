72aebe7f8e945cf42e84a2249d68844456467c0a
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 05:46:29 +0900

    Title: 씨 오 더블유 2, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240507/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,12 @@
+n=int(input())
+s=input()
+answer=0
+
+for i in range(n-2):
+    if s[i]=='C':
+        for j in range(i+1,n-1):
+            if s[j]=='O':
+                for k in range(j+1,n):
+                    if s[k]=='W':
+                        answer+=1
+print(answer)

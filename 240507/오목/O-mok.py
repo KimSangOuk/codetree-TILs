3f71d07326db9cca3804b89f6694df01aa5e46e6
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 06:16:34 +0900

    Title: 오목, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/오목/O-mok.py
+++ 240507/오목/O-mok.py
@@ -15,6 +15,14 @@ for i in range(19-4):
             answer=1
             mid_pos_x,mid_pos_y=i+3,j+1
             break
+        elif board[i][j]==1 and board[i+1][j+1]==1 and board[i+2][j+2]==1 and board[i+3][j+3]==1 and board[i+4][j+4]==1:
+            answer=1
+            mid_pos_x,mid_pos_y=i+3,j+3
+            break
+        elif board[i-4][j]==1 and board[i-3][j+1]==1 and board[i-2][j+2]==1 and board[i-1][j+3]==1 and board[i][j+4]==1:
+            answer=1
+            mid_pos_x,mid_pos_y=i-1,j+3
+            break
         elif board[i][j]==2 and board[i][j+1]==2 and board[i][j+2]==2 and board[i][j+3]==2 and board[i][j+4]==2:
             answer=2
             mid_pos_x,mid_pos_y=i+1,j+3
@@ -22,6 +30,14 @@ for i in range(19-4):
         elif board[i][j]==2 and board[i+1][j]==2 and board[i+2][j]==2 and board[i+3][j]==2 and board[i+4][j]==2:
             answer=2
             mid_pos_x,mid_pos_y=i+3,j+1
+            break
+        elif board[i][j]==2 and board[i+1][j+1]==2 and board[i+2][j+2]==2 and board[i+3][j+3]==2 and board[i+4][j+4]==2:
+            answer=2
+            mid_pos_x,mid_pos_y=i+3,j+3
+            break
+        elif board[i-4][j]==2 and board[i-3][j+1]==2 and board[i-2][j+2]==2 and board[i-1][j+3]==2 and board[i][j+4]==2:
+            answer=2
+            mid_pos_x,mid_pos_y=i-1,j+3
             break
     if answer!=0:
         break

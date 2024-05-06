16a106cc75c34337b02e4102c5b411d7f2ec2b23
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 06:54:20 +0900

    Title: 최고의 13위치 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/최고의 13위치 2/best-place-of-13-2.py
+++ 240507/최고의 13위치 2/best-place-of-13-2.py
@@ -6,14 +6,16 @@ for i in range(n):
 answer=0
 for i in range(n):
     for j in range(n-2):
-        sum_value=board[i][j]+board[i][j+1]+board[i][j+2]
         for k in range(n):
             if k==i:
                 for t in range(j+3,n-2):
-                    sum_value+=board[k][t]+board[k][t+1]+board[k][t+2]
+                    sum_value=0
+                    sum_value+=board[i][j]+board[i][j+1]+board[i][j+2]+board[k][t]+board[k][t+1]+board[k][t+2]
+                    answer=max(sum_value,answer)
             else:
                 for t in range(n-2):
-                    sum_value+=board[k][t]+board[k][t+1]+board[k][t+2]
-        answer=max(sum_value,answer)
+                    sum_value=0
+                    sum_value+=board[i][j]+board[i][j+1]+board[i][j+2]+board[k][t]+board[k][t+1]+board[k][t+2]
+                    answer=max(sum_value,answer)
 
 print(answer)

6659f97d21efab7fc802b4ef0a8ff110462dd8f7
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 05:56:54 +0900

    Title: Carry 피하기 2, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240507/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,25 @@
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(int(input()))
+
+answer=0
+for i in range(n-2):
+    for j in range(n-1):
+        for k in range(n):
+            first=str(arr[i]).zfill(4)
+            second=str(arr[j]).zfill(4)
+            third=str(arr[k]).zfill(4)
+
+            carry=False
+            for t in range(4):
+                if int(first[t])+int(second[t])+int(third[t])>=10:
+                    carry=True
+                    break
+            if not carry:
+                answer=max(arr[i]+arr[j]+arr[k],answer)
+
+if answer==0:
+    print(-1)
+else:
+    print(answer)

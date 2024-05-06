728e64f899a15230142239898db463a6a38adc5b
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 06:00:58 +0900

    Title: Carry 피하기 2, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/Carry 피하기 2/escaping-carry-2.py
+++ 240507/Carry 피하기 2/escaping-carry-2.py
@@ -5,8 +5,8 @@ for i in range(n):
 
 answer=0
 for i in range(n-2):
-    for j in range(n-1):
-        for k in range(n):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
             first=str(arr[i]).zfill(4)
             second=str(arr[j]).zfill(4)
             third=str(arr[k]).zfill(4)

a68aa3216d9ae94b1ba6359a3a18f97c94aa4f3e
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 06:19:18 +0900

    Title: 오목, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/오목/O-mok.py
+++ 240507/오목/O-mok.py
@@ -21,7 +21,7 @@ for i in range(19-4):
             break
         elif board[i+4][j]==1 and board[i+3][j+1]==1 and board[i+2][j+2]==1 and board[i+1][j+3]==1 and board[i][j+4]==1:
             answer=1
-            mid_pos_x,mid_pos_y=i-1,j+3
+            mid_pos_x,mid_pos_y=i+3,j+3
             break
         elif board[i][j]==2 and board[i][j+1]==2 and board[i][j+2]==2 and board[i][j+3]==2 and board[i][j+4]==2:
             answer=2
@@ -37,7 +37,7 @@ for i in range(19-4):
             break
         elif board[i+4][j]==2 and board[i+3][j+1]==2 and board[i+2][j+2]==2 and board[i+1][j+3]==2 and board[i][j+4]==2:
             answer=2
-            mid_pos_x,mid_pos_y=i-1,j+3
+            mid_pos_x,mid_pos_y=i+3,j+3
             break
     if answer!=0:
         break

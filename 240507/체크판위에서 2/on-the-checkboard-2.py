617ed61f8e08e085df7e12a8da314084759f8b3e
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-07 04:54:53 +0900

    Title: 체크판위에서 2, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/체크판위에서 2/on-the-checkboard-2.py
+++ 240507/체크판위에서 2/on-the-checkboard-2.py
@@ -6,12 +6,13 @@ for i in range(r):
 now=board[0][0]
 
 answer=0
-for i in range(1,r):
-    for j in range(1,c):
-        if board[i][j]!=now:
-            for k in range(i+1,r-1):
-                for t in range(j+1,c-1):
-                    if board[k][t]==now:
-                        answer+=1
+if board[0][0]!=board[r-1][c-1]:
+    for i in range(1,r):
+        for j in range(1,c):
+            if board[i][j]!=now:
+                for k in range(i+1,r-1):
+                    for t in range(j+1,c-1):
+                        if board[k][t]==now:
+                            answer+=1
 
 print(answer)

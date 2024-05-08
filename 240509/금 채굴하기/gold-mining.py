84ea71610b881ba783297cb4eafa8f6fe7c46400
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 00:55:08 +0900

    Title: 금 채굴하기, Time: 216ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240509/금 채굴하기/gold-mining.py
+++ 240509/금 채굴하기/gold-mining.py
@@ -11,6 +11,13 @@ for i in range(n):
         if arr[j]==1:
             coins_cnt+=1
 
+max_k=0
+for i in range(n):
+    if i*i+(i+1)*(i+1)<m*coins_cnt:
+        max_k=i
+    else:
+        break
+
 dx=[0,0,-1,1]
 dy=[1,-1,0,0]
 
@@ -22,7 +29,6 @@ def bfs(board,x,y,dist):
     q.append((x,y))
     visited[x][y]=0
     cnt=board[x][y]
-    total_cnt=1
     while q:
         n_x,n_y=q.popleft()
         if visited[n_x][n_y]>=dist:
@@ -37,17 +43,13 @@ def bfs(board,x,y,dist):
             visited[nx][ny]=visited[n_x][n_y]+1
             q.append((nx,ny))
             cnt+=board[nx][ny]
-            total_cnt+=1
-
-    if total_cnt<=m*cnt:
-        return cnt
-    else:
-        return 0
+    return cnt
 
 for i in range(n):
     for j in range(n):
-        for k in range(n*2):
+        for k in range(max_k+1):
             cnt=bfs(board,i,j,k)
-            answer=max(cnt,answer)
+            if k*k+(k+1)*(k+1)<=m*cnt:
+                answer=max(cnt,answer)
 
 print(answer)

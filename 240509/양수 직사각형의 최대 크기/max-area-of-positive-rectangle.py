5b4cc6113b2ffefb6e9291b29579bc848c44aa9c
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 03:26:41 +0900

    Title: 양수 직사각형의 최대 크기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240509/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -10,9 +10,11 @@ for h in range(1,n+1):
         all_positive=True
         for x in range(n-h+1):
             for y in range(m-w+1):
-                if board[x][y]<=0:
-                    all_positive=False
-                    break
+                for a in range(x,x+h):
+                    for b in range(y,y+w):
+                        if board[a][b]<=0:
+                            all_positive=False
+                            break
         if all_positive:
             answer=max(answer,h*w)
 print(answer)

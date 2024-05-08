c301caae78fbb5fbd04a65a12f10ba075d6b06a3
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 00:41:10 +0900

    Title: 금 채굴하기, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240509/금 채굴하기/gold-mining.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+n,m=map(int,input().split())
+board=list()
+
+coins_cnt=0
+for i in range(n):
+    arr=list(map(int,input().split()))
+    board.append(arr)
+    for j in range(n):
+        if arr[j]==1:
+            coins_cnt+=1
+
+max_k=0
+for i in range(n):
+    if i*i+(i+1)*(i+1)<m*coins_cnt:
+        max_k=i
+    else:
+        break
+
+dx=[0,0,-1,1]
+dy=[1,-1,0,0]
+
+answer=0
+
+def bfs(board,x,y,dist):
+    q=deque()
+    visited=[[-1]*n for _ in range(n)]
+    q.append((x,y))
+    visited[x][y]=0
+    cnt=board[x][y]
+    while q:
+        n_x,n_y=q.popleft()
+        if visited[n_x][n_y]>=dist:
+            break
+        for i in range(4):
+            nx=n_x+dx[i]
+            ny=n_y+dy[i]
+            if nx<0 or ny<0 or nx>=n or ny>=n:
+                continue
+            if visited[nx][ny]!=-1:
+                continue
+            visited[nx][ny]=visited[n_x][n_y]+1
+            q.append((nx,ny))
+            cnt+=board[nx][ny]
+    return cnt
+
+for i in range(n):
+    for j in range(n):
+        for k in range(max_k+1):
+            cnt=bfs(board,i,j,k)
+            if k*k+(k+1)*(k+1)<=m*cnt:
+                answer=max(cnt,answer)
+
+print(answer)

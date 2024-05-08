13181496c0d51e87879d56ec970497383bef98d5
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-08 19:11:22 +0900

    Title: 아름다운 수열 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240508/아름다운 수열 2/beautiful-sequence-2.py
+++ 240508/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,15 +1,15 @@
 n,m=map(int,input().split())
 
 arr_a=list(map(int,input().split()))
-arr_b=set(list(map(int,input().split())))
+arr_b=list(map(int,input().split()))
+arr_b.sort()
 
 answer=0
 for i in range(n-m+1):
-    tf=True
-    for k in arr_a[i:i+m]:
-        if k not in arr_b:
-            tf=False
-    if tf:
+    arr=arr_a[i:i+m]
+    arr.sort()
+
+    if arr==arr_b:
         answer+=1
 
 print(answer)

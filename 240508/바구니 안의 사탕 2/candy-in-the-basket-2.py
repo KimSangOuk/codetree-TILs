22ba5a816bfc0f4d8d9f9a14cc2690e43596352c
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-08 19:18:49 +0900

    Title: 바구니 안의 사탕 2, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240508/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,12 @@
+n,k=map(int,input().split())
+arr=[0]*101
+
+for i in range(n):
+    candy,basket=map(int,input().split())
+    arr[basket]=candy
+
+answer=0
+for i in range(101-2*k):
+    answer=max(answer,sum(arr[i:i+2*k+1]))
+
+print(answer)

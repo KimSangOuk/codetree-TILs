d6c0ea4d2ce12d7d82e5a5e9e1648fa42292d2c2
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 15:32:12 +0900

    Title: 스승의 은혜 3, Time: 163ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240509/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240509/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -2,20 +2,20 @@ n,b=map(int,input().split())
 students=list()
 for i in range(n):
     p,s=map(int,input().split())
-    students.append((p,s))
+    students.append([p,s])
 
-students.sort(key=lambda x:x[0]+x[1])
+
 
 answer=0
 for i in range(n):
     cost=0
     cnt=0
+    students[i][0]//=2
+    students.sort(key=lambda x:x[0]+x[1])
     for j in range(n):
-        if i==j:
-            cost+=students[j][0]//2+students[j][1]
-        else:
-            cost+=students[j][0]+students[j][1]
+        cost+=students[j][0]+students[j][1]
         if cost<=b:
             cnt+=1
+    students[i][0]*=2
     answer=max(answer,cnt)
 print(answer)

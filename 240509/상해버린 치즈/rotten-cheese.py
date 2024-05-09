3276e7b30d67cc18f0010299a9dd8593d6009103
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 13:17:57 +0900

    Title: 상해버린 치즈, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/상해버린 치즈/rotten-cheese.py
+++ 240509/상해버린 치즈/rotten-cheese.py
@@ -1,7 +1,9 @@
 n,m,d,s=map(int,input().split())
 eat_data=[]
 sick_data=[]
-cheeses=[0]*m
+cheeses=dict()
+for i in range(m):
+    cheeses[i]=[]
 for i in range(d):
     eat_data.append(tuple(map(int,input().split())))
 for i in range(s):
@@ -12,11 +14,11 @@ for i in range(s):
     for j in range(d):
         ep,em,et=eat_data[j]
         if ep==p and et<t:
-            cheeses[em-1]+=1
+            cheeses[em-1].append(ep)
 
 sick_check=[0]*n
 for i in range(m):
-    if cheeses[i]==s:
+    if len(set(cheeses[i]))==s:
         for j in range(d):
             ep,em,et=eat_data[j]
             if em==i+1:

186af0b393223a6a63b2e8b52168e7a1528da4d1
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 16:46:02 +0900

    Title: 등차수열, Time: 193ms, Memory: 72MB, Status: Passed - CodeTree

--- 240509/등차수열/arithmetic-sequence.py
+++ 240509/등차수열/arithmetic-sequence.py
@@ -3,9 +3,11 @@ arr=list(map(int,input().split()))
 
 answer=0
 for i in range(1,101):
+    cnt=0
     for j in range(len(arr)-1):
         for k in range(j+1,len(arr)):
             if arr[j]<=i<=arr[k] or arr[k]<=i<=arr[j]:
                 if abs(arr[j]-i)==abs(arr[k]-i):
-                    answer+=1
+                    cnt+=1
+    answer=max(cnt,answer)
 print(answer)

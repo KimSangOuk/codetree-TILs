1183d89e58f617e503865dd000fbbc1603736df8
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-09 13:44:06 +0900

    Title: 이상한 폭탄 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,11 @@
+n,k=map(int,input().split())
+
+arr=list()
+for i in range(n):
+    arr.append(int(input()))
+
+answer=0
+for i in range(n):
+    if arr[i] in arr[i+1:i+k+1]:
+        answer=max(answer,arr[i])
+print(answer)

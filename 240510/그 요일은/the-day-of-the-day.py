3d8cdf3cbf95597ae0c6b8a71437a0b705c4a59b
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-10 15:22:05 +0900

    Title: 그 요일은, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240510/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,20 @@
+m1,d1,m2,d2=map(int,input().split())
+a=input()
+months=[31,29,31,30,31,30,31,31,30,31,30,31]
+
+day_of_the_week=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+
+answer=0
+pass_day=0
+dow=0
+while True:
+    if m1==m2 and d1==d2:
+        break
+    d1+=1
+    dow=(dow+1)%7
+    if d1>months[m1]:
+        d1=1
+        m1+=1
+    if day_of_the_week[dow]==a:
+        answer+=1
+print(answer)

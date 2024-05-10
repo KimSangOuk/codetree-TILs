1a3d8fd8dd78ed962bc35085dce776bf2114dd18
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-10 12:30:27 +0900

    Title: 3개의 선 2, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240510/3개의 선 2/three-lines-2.py
+++ 240510/3개의 선 2/three-lines-2.py
@@ -4,26 +4,29 @@ points=[]
 for i in range(n):
     points.append(tuple(map(int,input().split())))
 
-line=[]
-
+x_set=set()
+y_set=set()
 for i in range(n):
-    find=False
-    for j in range(len(line)):
-        if line[j][0]==points[i][0] or line[j][1]==points[i][1]:
-            find=True
-            break
-    if find:
-        continue
-    for j in range(n):
-        if i!=j:
-            if points[i][0]==points[j][0]:
-                line.append((points[i][0],-1))
-                break
-            elif points[j][1]==points[i][1]:
-                line.append((-1,points[i][1]))
-                break
+    x_set.add(points[i][0])
+    y_set.add(points[i][1])
 
-if len(line)>3:
-    print(0)
-else:
-    print(1)
+line=[]
+for i in x_set:
+    line.append((i,-1))
+for i in y_set:
+    line.append((-1,i))
+
+answer=0
+length=len(line)
+for i in range(length-2):
+    for j in range(i+1,length-1):
+        for k in range(j+1,length):
+            tf=True
+            for a,b in points:
+                if (line[i][0]==a or line[i][1]==b) or (line[j][0]==a or line[j][1]==b) or (line[k][0]==a or line[k][1]==b):
+                    continue
+                else:
+                    tf=False
+            if tf:
+                answer=1
+print(answer)

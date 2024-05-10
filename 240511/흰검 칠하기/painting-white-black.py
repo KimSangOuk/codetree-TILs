0f2dc334199acf3a00449516bd3aad598ff9a44c
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-11 02:30:52 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240511/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,33 @@
+n=int(input())
+arr_w=[0]*101
+arr_b=[0]*101
+arr_end=[0]*101
+
+start=50
+for i in range(n):
+    dict,dir=input().split()
+    dict=int(dict)
+    for j in range(dict):
+        if dir=='R':
+            arr_b[start+j]+=1
+            arr_end[start+j]=1
+        else:
+            arr_w[start-j]+=1
+            arr_end[start-j]=2
+    if dir=='R':
+        start+=dict-1
+    else:
+        start-=dict-1
+
+w_cnt=0
+b_cnt=0
+g_cnt=0
+for i in range(101):
+    if arr_b[i]>=2 and arr_w[i]>=2:
+        g_cnt+=1
+    elif arr_end[i]==1:
+        b_cnt+=1
+    elif arr_end[i]==2:
+        w_cnt+=1
+
+print(w_cnt,b_cnt,g_cnt)

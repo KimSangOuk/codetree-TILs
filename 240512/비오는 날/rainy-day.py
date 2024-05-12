5a928352abb8c38070f46da2af3d171f095e120e
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-12 14:17:36 +0900

    Title: 비오는 날, Time: 168ms, Memory: 26MB, Status: Passed - CodeTree

--- 240512/비오는 날/rainy-day.py
+++ 240512/비오는 날/rainy-day.py
@@ -9,14 +9,15 @@ class Data:
         self.weather=weather
 
 data_list=list()
-recent_date="2101-12-31"
+recent_date="3000-12-31"
 recent_index=-1
 for i in range(n):
     date,dow,weather=map(str,input().split())
+    data_list.append(Data(date,dow,weather))
     a=datetime(int(recent_date[0:4]),int(recent_date[5:7]),int(recent_date[8:]))
     b=datetime(int(date[0:4]),int(date[5:7]),int(date[8:]))
-    if a<b and weather=="Rain":
+    if a>b and weather=="Rain":
         recent_date=date
         recent_index=i
 
-print(data_list[i].date,data_list[i].dow,data_list[i].weather)
+print(data_list[recent_index].date,data_list[recent_index].dow,data_list[recent_index].weather)

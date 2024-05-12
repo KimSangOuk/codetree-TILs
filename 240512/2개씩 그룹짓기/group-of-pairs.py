5c06f5d21f50fa2bde4e67154f1c73416c1b11bc
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-12 12:30:41 +0900

    Title: 2개씩 그룹짓기, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240512/2개씩 그룹짓기/group-of-pairs.py
+++ 240512/2개씩 그룹짓기/group-of-pairs.py
@@ -1,9 +1,8 @@
 n=int(input())
 arr=list(map(int,input().split()))
 arr.sort()
-a,b=0,0
-for i in range(n//2):
-    a+=arr[i]+arr[n*2-i-1]
-for i in range(n//2,n):
-    b+=arr[i]+arr[n*2-i-1]
-print(max(a,b))
+answer=0
+for i in range(n):
+    a=arr[i]+arr[n*2-i-1]
+    answer=max(answer,a)
+print(answer)

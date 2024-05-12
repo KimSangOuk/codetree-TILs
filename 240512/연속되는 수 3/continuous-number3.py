b71ec1485f6fe4372a679b682247bf56c04fadde
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-12 10:44:49 +0900

    Title: 연속되는 수 3, Time: 156ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240512/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(int(input()))
+
+cnt=0
+answer=0
+for i in range(n):
+    if i==0 or (arr[i-1]>0 and arr[i]<0) or (arr[i-1]<0 and arr[i]>0):
+        cnt=1
+    else:
+        cnt+=1
+        answer=max(answer,cnt)
+print(answer)

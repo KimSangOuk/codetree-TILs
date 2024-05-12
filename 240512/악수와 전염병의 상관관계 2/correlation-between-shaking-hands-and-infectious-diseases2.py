bbf5e6a20fd1e17e144c02a02ac7f4787fa623a8
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-12 11:57:02 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240512/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240512/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,15 +1,25 @@
 n,k,p,t=map(int,input().split())
 get_cold=[0]*(n+1)
+cnt_put=[p]*(n+1)
 record=list()
 for i in range(t):
     record.append(list(map(int,input().split())))
 get_cold[p]=1
 record.sort()
 for i in range(t):
-    if p>0 and (get_cold[record[i][1]]==1 or get_cold[record[i][2]]==1):
-        p-=1
-        get_cold[record[i][1]]=1
-        get_cold[record[i][2]]=1
+    if get_cold[record[i][1]]==1 and get_cold[record[i][2]]==1:
+        if cnt_put[record[i][1]]>0:
+            cnt_put[record[i][1]]-=1
+        if cnt_put[record[i][2]]>0:
+            cnt_put[record[i][2]]-=1
+    elif get_cold[record[i][1]]==1:
+        if cnt_put[record[i][1]]>0:
+            cnt_put[record[i][1]]-=1
+            get_cold[record[i][2]]=1
+    elif get_cold[record[i][2]]==1:
+        if cnt_put[record[i][2]]>0:
+            cnt_put[record[i][2]]-=1
+            get_cold[record[i][1]]=1
 
 for i in range(1,n+1):
     print(get_cold[i],end='')

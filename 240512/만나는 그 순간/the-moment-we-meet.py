c6b6a12390d9a3390aebb41cfcf06b7f50cd5818
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-12 11:09:35 +0900

    Title: 만나는 그 순간, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240512/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,26 @@
+n,m=map(int,input().split())
+
+a,b=[0],[0]
+a_pos,b_pos=0,0
+for i in range(n):
+    d,t=input().split()
+    for k in range(int(t)):
+        if d=='R':
+            a_pos+=1
+        elif d=='L':
+            a_pos-=1
+        a.append(a_pos)
+for i in range(m):
+    d,t=input().split()
+    for k in range(int(t)):
+        if d=='R':
+            b_pos+=1
+        elif d=='L':
+            b_pos-=1
+        b.append(b_pos)
+answer=0
+for i in range(1,len(a)):
+    if a[i]==b[i]:
+        answer=i
+        break
+print(answer)

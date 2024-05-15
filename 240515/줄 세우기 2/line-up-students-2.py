743446c21afdb30e7977caeb22677dde87f09d19
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-15 11:57:10 +0900

    Title: 줄 세우기 2, Time: 180ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240515/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,8 @@
+n=int(input())
+arr=[]
+for i in range(1,n+1):
+    h,w=map(int,input().split())
+    arr.append((h,w,i))
+arr.sort(key=lambda x:(x[0],-x[1]))
+for i in range(n):
+    print(*arr[i])

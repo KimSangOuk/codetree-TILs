56269874777552c5a5747e0e58433f5f1df8a23c
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-15 11:09:12 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240515/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,8 @@
+n=int(input())
+arr=[]
+for i in range(n):
+    name,h,w=map(str,input().split())
+    arr.append((name,int(h),int(w)))
+arr.sort(key=lambda x:(x[1],-x[2]))
+for i in range(n):
+    print(*arr[i])

8099455863f78df83e310d6dedfd9eafaec80e97
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-15 13:45:35 +0900

    Title: 독서실의 거리두기 2, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240515/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,34 @@
+n=int(input())
+arr=list(input())
+for i in range(n):
+    arr[i]=int(arr[i])
+last_index=0
+max_index_1_1=0
+max_length_1_1=0
+for i in range(1,n):
+    if arr[i]==1:
+        if max_length_1_1<i-last_index-1:
+            max_length_1_1=i-last_index-1
+            max_index_1_1=i
+        last_index=i
+end_zero_length=0
+if arr[-1]==0:
+    for i in range(n-1,-1,-1):
+        if arr[i]==0:
+            end_zero_length+=1
+        else:
+            break
+find_index=-1
+if end_zero_length-1>max_length_1_1//2:
+    find_index=n-1
+else:
+    find_index=max_index_1_1-(max_length_1_1+1)//2
+arr[find_index]=1
+answer=n
+last_index=0
+# print(arr)
+for i in range(1,n):
+    if arr[i]==1:
+        answer=min(answer,i-last_index)
+        last_index=i
+print(answer)

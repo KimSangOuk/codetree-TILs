62f897c7c533625082f83feb554bcb2f8b1f86af
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-16 20:29:40 +0900

    Title: 세 수의 최대 곱, Time: 149ms, Memory: 31MB, Status: Passed - CodeTree

--- 240516/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 240516/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -2,12 +2,29 @@ n=int(input())
 arr=list(map(int,input().split()))
 yang=[]
 eum=[]
+zero=False
 for i in range(n):
     if arr[i]>0:
         yang.append(arr[i])
     elif arr[i]<0:
         eum.append(arr[i])
+    elif arr[i]==0:
+        zero=True
 yang.sort(reverse=True)
 eum.sort()
-answer=max(yang[0]*yang[1]*yang[2],eum[0]*eum[1]*yang[0])
-print(answer)
+possible=[]
+if len(yang)>=3:
+    possible.append(yang[0]*yang[1]*yang[2])
+if len(yang)>=2 and len(eum)>=1:
+    if zero:
+        possible.append(0)
+    else:
+        possible.append(yang[-1]*yang[-2]*eum[-1])
+if len(yang)>=1 and len(eum)>=2:
+    possible.append(yang[0]*eum[0]*eum[1])
+if len(eum)>=3:
+    if zero:
+        possible.append(0)
+    else:
+        possible.append(eum[-1]*eum[-2]*eum[-3]) 
+print(max(possible))

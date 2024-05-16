dfb91272ac8aaea574b690ab002f07e27659d2c3
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-16 19:59:49 +0900

    Title: 두 번째로 작은 수의 위치, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,10 @@
+import copy
+
+n=int(input())
+arr=list(map(int,input().split()))
+find_arr=copy.deepcopy(arr)
+arr.sort()
+for i in range(1,n+1):
+    if find_arr[i-1]==arr[1]:
+        print(i)
+        break

91eadc09c4522cefcee5b7097a98ec1bfee8fbd1
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-16 20:00:51 +0900

    Title: 두 번째로 작은 수의 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240516/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240516/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -3,7 +3,7 @@ import copy
 n=int(input())
 arr=list(map(int,input().split()))
 find_arr=copy.deepcopy(arr)
-arr.sort()
+arr=sorted(list(set(arr)))
 for i in range(1,n+1):
     if find_arr[i-1]==arr[1]:
         print(i)

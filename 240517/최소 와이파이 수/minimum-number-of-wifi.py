f3aa283a2e1f6ea252ba02e97899933a7b7da86a
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-17 08:39:29 +0900

    Title: 최소 와이파이 수, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240517/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240517/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,15 +2,21 @@ n,m=map(int,input().split())
 arr=list(map(int,input().split()))
 answer=0
 last_cnt=1+m*2
+start=False
 for i in range(n):
     # print(i,last_dist,answer)
-    if i==0 and arr[i]==1 and last_cnt==m*2+1:
+    if arr[i]==1 and last_cnt==m*2+1:
         last_cnt-=1
         answer+=1
+        start=True
     elif arr[i]==1 and last_cnt==0:
         answer+=1
         last_cnt=m*2
-    elif m*2+1>=last_cnt>0:
+        start=True
+    elif start and m*2+1>=last_cnt>0:
         last_cnt-=1
+    elif arr[i]==0 and last_cnt==0:
+        start=False
+        last_cnt=m*2+1
     # print(i,last_cnt,answer)
 print(answer)

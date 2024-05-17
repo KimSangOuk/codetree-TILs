f6869299a5d8f32021e903724b6bbbbe6659460e
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-17 12:41:46 +0900

    Title: 원소의 합이 0, Time: 5026ms, Memory: 51MB, Status: Time Limit Exceed - CodeTree

--- 240517/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240517/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -3,27 +3,26 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 c=list(map(int,input().split()))
 d=list(map(int,input().split()))
-cnt=dict()
-case_a_b=[]
-case_c_d=[]
+case_a_b=dict()
+case_c_d=dict()
 for i in range(n):
     for j in range(n):
-        if a[i]+b[j] in cnt:
-            cnt[a[i]+b[j]]+=1
+        if a[i]+b[j] in case_a_b:
+            case_a_b[a[i]+b[j]]+=1
         else:
-            cnt[a[i]+b[j]]=1
+            case_a_b[a[i]+b[j]]=1
 for i in range(n):
     for j in range(n):
-        if c[i]+d[j] in cnt:
-            cnt[c[i]+d[j]]+=1
+        if c[i]+d[j] in case_c_d:
+            case_c_d[c[i]+d[j]]+=1
         else:
-            cnt[c[i]+d[j]]=1
+            case_c_d[c[i]+d[j]]=1
 answer=0
-for i in list(set(cnt.keys())):
-    if i==100-i:
-        if i in cnt:
-            answer+=(cnt[i])*(cnt[i]-1)
-    else:
-        if i in cnt and 100-i in cnt:
-            answer+=(cnt[i])*(cnt[100-i])
-print(answer//2)
+for i in list(set(case_a_b.keys())):
+    for j in list(set(case_c_d.keys())):
+        if i==j and i==0:
+            answer+=(case_a_b[i])*(case_c_d[j])
+        else:
+            if i+j==0:
+                answer+=(case_a_b[i])*(case_c_d[j])
+print(answer)

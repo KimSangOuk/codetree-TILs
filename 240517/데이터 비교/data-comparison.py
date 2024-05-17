650bf9ebd64352e01a0f040d0b29221a04254d30
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-17 19:45:52 +0900

    Title: 데이터 비교, Time: 5072ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240517/데이터 비교/data-comparison.py
+++ 240517/데이터 비교/data-comparison.py
@@ -1,7 +1,8 @@
+import sys
 n=int(input())
-arr1=list(map(int,input().split()))
+arr1=list(map(int,sys.stdin.readline().split()))
 m=int(input())
-arr2=list(map(int,input().split()))
+arr2=list(map(int,sys.stdin.readline().split()))
 
 for i in arr2:
     if i in set(arr1):

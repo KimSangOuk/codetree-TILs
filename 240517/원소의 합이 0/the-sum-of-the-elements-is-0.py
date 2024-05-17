c82c5e1c1bba2283d373a7d279fb23ebb70e974b
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-17 12:33:19 +0900

    Title: 원소의 합이 0, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240517/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240517/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -3,17 +3,27 @@ a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 c=list(map(int,input().split()))
 d=list(map(int,input().split()))
-answer=0
+cnt=dict()
 case_a_b=[]
 case_c_d=[]
 for i in range(n):
     for j in range(n):
-        case_a_b.append(a[i]+b[j])
+        if a[i]+b[j] in cnt:
+            cnt[a[i]+b[j]]+=1
+        else:
+            cnt[a[i]+b[j]]=1
 for i in range(n):
     for j in range(n):
-        case_c_d.append(c[i]+d[j])
-for i in range(len(case_a_b)):
-    for j in range(len(case_c_d)):
-        if case_a_b[i]+case_c_d[j]==0:
-            answer+=1
-print(answer)
+        if c[i]+d[j] in cnt:
+            cnt[c[i]+d[j]]+=1
+        else:
+            cnt[c[i]+d[j]]=1
+answer=0
+for i in list(set(cnt.keys())):
+    if i==100-i:
+        if i in cnt:
+            answer+=(cnt[i])*(cnt[i]-1)
+    else:
+        if i in cnt and 100-i in cnt:
+            answer+=(cnt[i])*(cnt[100-i])
+print(answer//2)

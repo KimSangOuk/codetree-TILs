022ef5a8cac18bdc3c202b93ca8bc7e14346405d
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-17 12:24:51 +0900

    Title: 원소의 합이 0, Time: 1245ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240517/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -0,0 +1,12 @@
+from itertools import product
+
+n=int(input())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=list(map(int,input().split()))
+d=list(map(int,input().split()))
+answer=0
+for arr in list(product(a,b,c,d)):
+    if sum(arr)==0:
+        answer+=1
+print(answer)

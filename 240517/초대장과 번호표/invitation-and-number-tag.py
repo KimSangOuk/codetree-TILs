850168a02038c60a784ebee9aba7f9905189f6af
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-17 20:46:44 +0900

    Title: 초대장과 번호표, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240517/초대장과 번호표/invitation-and-number-tag.py
+++ 240517/초대장과 번호표/invitation-and-number-tag.py
@@ -5,10 +5,14 @@ for i in range(g):
     arr=list(map(int,input().split()))
     group.append(sorted(arr[1:]))
 
-group.sort()
+group.sort(key=lambda x:len(x))
+# print(group)
 for k in group:
+    cnt=0
     for j in k:
-        if j not in invited:
-            invited.add(j)
-            break
+        if j in invited:
+            cnt+=1
+    if cnt==len(k)-1:
+        invited=invited|set(k)
+# print(invited)
 print(len(list(invited)))

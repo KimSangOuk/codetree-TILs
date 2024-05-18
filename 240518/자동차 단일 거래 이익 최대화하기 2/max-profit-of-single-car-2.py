0630e672db43cbe266983717c26ce878c576c70f
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 16:27:19 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 115ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240518/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 240518/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -4,9 +4,9 @@ arr=list(map(int,input().split()))
 value=arr[0]
 answer=0
 for i in range(1,n):
-    if arr[i]-value<=0:
+    answer=max(answer,arr[i]-value)
+    if arr[i]-value<0:
         answer=0
         value=arr[i]
-    else:
-        answer=max(answer,arr[i]-value)
+        
 print(answer)

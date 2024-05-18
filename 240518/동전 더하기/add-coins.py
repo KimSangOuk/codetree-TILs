5f6dea6b409a62bfc5abadd63b6bd9fcb177f0da
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 14:41:34 +0900

    Title: 동전 더하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240518/동전 더하기/add-coins.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+coins=[]
+for i in range(n):
+    coins.append(int(input()))
+coins.sort(reverse=True)
+answer=0
+for coin in coins:
+    answer+=k//coin
+    k%=coin
+print(answer)

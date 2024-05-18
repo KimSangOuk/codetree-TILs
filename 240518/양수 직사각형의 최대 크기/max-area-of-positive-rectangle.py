cc4d98fbb7bac02ddf26bb5b2de29effeceec7d7
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 14:34:59 +0900

    Title: 양수 직사각형의 최대 크기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240518/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 240518/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -6,8 +6,8 @@ answer=0
 for i in range(1,n+1):
     for j in range(1,m+1):
         h,w=i,j
-        for s in range(n-h+1):
-            for t in range(m-w+1):
+        for s in range(n-h):
+            for t in range(m-w):
                 tf=True
                 for k in range(s,s+h):
                     for q in range(t,t+w):

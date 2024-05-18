45713bb56b74cf42be6461c417ef340787c3a2ab
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 16:18:24 +0900

    Title: 수 채우기, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/수 채우기/fill-in-number.py
+++ 240518/수 채우기/fill-in-number.py
@@ -1,13 +1,20 @@
 n=int(input())
 answer=0
-dp=[0]*100001
-dp[2]=1
-dp[3]=0
-dp[4]=2
-dp[5]=1
-for i in range(6,n+1):
-    dp[i]=min(dp[i-2],dp[i-5])+1
-if dp[n]==0:
-    print(-1)
+
+cnt5=n//5
+cnt2=0
+answer=0
+while True:
+    if cnt5<0:
+        answer=-1
+    if (n-cnt5*5)==0:
+        break
+    elif (n-cnt5*5)%2==0:
+        cnt2=(n-cnt5*5)//2
+        break
+    elif (n-cnt5*5)%2!=0:
+        cnt5-=1
+if answer==-1:
+    print(answer)
 else:
-    print(dp[n])
+    print(cnt5+cnt2)

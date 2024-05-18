01fee7ade28f7fccebbc604dc0998aea75395234
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 11:14:45 +0900

    Title: 앞에서부터 삭제하기 2, Time: 136ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240518/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240518/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,9 +3,13 @@ n=int(input())
 arr=list(map(int,input().split()))
 
 answer=0
-for i in range(1,n-1):
+for i in range(n-2,0,-1):
     tmp=arr[i:]
     heapq.heapify(tmp)
-    heapq.heappop(tmp) 
-    answer=max(sum(tmp)/len(tmp),answer)
+    heapq.heappop(tmp)
+    avg=sum(tmp)/len(tmp)
+    if avg<answer:
+        break
+    else:
+        answer=avg
 print("{:.2f}".format(answer))

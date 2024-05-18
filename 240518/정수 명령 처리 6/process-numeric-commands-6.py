bdac533b684fa84c97ba60be2baacc5f338b3689
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 10:36:46 +0900

    Title: 정수 명령 처리 6, Time: 303ms, Memory: 28MB, Status: Passed - CodeTree

--- 240518/정수 명령 처리 6/process-numeric-commands-6.py
+++ 240518/정수 명령 처리 6/process-numeric-commands-6.py
@@ -5,7 +5,7 @@ q=[]
 for i in range(n):
     order=list(map(str,input().split()))
     if order[0]=='push':
-        heapq.heappush(q,int(order[1]))
+        heapq.heappush(q,-int(order[1]))
     elif order[0]=='size':
         print(len(q))
     elif order[0]=='empty':
@@ -14,6 +14,6 @@ for i in range(n):
         else:
             print(0)
     elif order[0]=='pop':
-        print(heapq.heappop(q))
+        print(-heapq.heappop(q))
     elif order[0]=='top':
-        print(q[0])
+        print(-q[0])

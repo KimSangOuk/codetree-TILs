9e7d0e276272c0279626c24acecc3a790c14877d
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 11:33:49 +0900

    Title: 앞에서부터 삭제하기 2, Time: 155ms, Memory: 30MB, Status: Passed - CodeTree

--- 240518/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240518/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -3,13 +3,30 @@ n=int(input())
 arr=list(map(int,input().split()))
 
 answer=0
-for i in range(n-2,0,-1):
-    tmp=arr[i:]
-    heapq.heapify(tmp)
-    heapq.heappop(tmp)
-    avg=sum(tmp)/len(tmp)
-    if avg<answer:
-        break
-    else:
-        answer=avg
+min_value=int(1e9)
+min_index=-1
+sum_value=0
+length=1
+if arr[-1]>arr[-2]:
+    min_index=n-2
+    min_value=arr[-2]
+    answer=arr[-1]
+    sum_value=arr[-1]
+else:
+    min_index=n-1
+    min_value=arr[-1]
+    answer=arr[-2]
+    sum_value=arr[-2]
+
+for i in range(n-3,0,-1):
+    if arr[i]<=min_value:
+        sum_value+=min_value
+        min_index=i
+        min_value=arr[i]
+        length+=1
+        answer=max(answer,sum_value/length)
+    elif arr[i]>min_value:
+        sum_value+=arr[i]
+        length+=1
+        answer=max(answer,sum_value/length)
 print("{:.2f}".format(answer))

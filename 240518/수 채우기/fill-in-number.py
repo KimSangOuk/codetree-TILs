2321a27aadc4147c2c7d72234b38e7184cfefe85
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-18 16:12:02 +0900

    Title: 수 채우기, Time: 109ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240518/수 채우기/fill-in-number.py
+++ 240518/수 채우기/fill-in-number.py
@@ -1,9 +1,11 @@
 n=int(input())
 answer=0
-dp=[int(1e9)]*100001
+dp=[0]*100001
 dp[2]=1
+dp[3]=0
+dp[4]=2
 dp[5]=1
-for i in range(3,n+1):
+for i in range(6,n+1):
     dp[i]=min(dp[i-2],dp[i-5])+1
 if dp[n]>=int(1e9):
     print(-1)

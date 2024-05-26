d851d69e3abd96f99bd8510c26ac8f80dee13b57
KimSangOuk <105284741+KimSangOuk@users.noreply.github.com>
2024-05-26 16:21:19 +0900

    Title: G & H 반전시키기 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240526/G & H 반전시키기 2/reversing-g-and-h-2.py
+++ 240526/G & H 반전시키기 2/reversing-g-and-h-2.py
@@ -8,7 +8,11 @@ for i in range(n-1,-1,-1):
     if not start and s[i]!=target[i]:
         cnt+=1
         start=True
+        for j in range(i+1):
+            if s[j]=='G':
+                s[j]='H'
+            else:
+                s[j]='G'
     elif start and s[i]==target[i]:
         start=False
-
-print(cnt+1)
+print(cnt)
